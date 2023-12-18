97f9b527374685f64d6f85465f0c4bff5456c88a
xiaohui <xh4010@163.com>
2023-12-18 13:06:06 +0800

    add examples

---
+++ examples/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+const path = require('path');
+const XlsxTemplater = require('../index')
+async function main() {
+  const template = path.join(__dirname, './resources/template.xlsx');
+  const xlsxTemplater = new XlsxTemplater(template);
+  await xlsxTemplater.parse();
+  await xlsxTemplater.render({
+    logo: path.join(__dirname, './resources/logo.png'),
+    code: 'https://www.baidu.com',
+    creator: 'xh4010',
+    creatAt: new Date(),
+    items: [{
+        name: "Product A",
+        quantity: 5,
+        price: 10,
+        desc:'',
+      },
+      {
+        name: "Product B",
+        quantity: 1,
+        price:  20,
+        desc:'note'
+      },
+    ],
+  }, {})
+  const pdfBuf = await xlsxTemplater.export();
+  const filePath = path.join(__dirname, './resources/output/report.pdf')
+  await fs.writeFile(filePath, pdfBuf)
+}
+
+main().catch(function (err) {
+  console.error('Error: ', err);
+});
